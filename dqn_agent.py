d899486667625deb868dd8e97b2a07894168055d
coiw2 <coiw2@lnx-n109-23.lunet.lboro.ac.uk>
2021-05-06 16:36:58 +0100

    improved distance reward calculation and stopped rewards being cumulative

--- dqn_agent.py
+++ dqn_agent.py
@@ -31,7 +31,7 @@ from turtlebot3_msgs.srv import Dqn
 from std_srvs.srv import Empty
 
 import tensorflow as tf
-from tensorflow.keras.layers import Dense
+from tensorflow.keras.layers import Dense, Dropout
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.models import load_model
 from tensorflow.keras.optimizers import Adam
@@ -79,12 +79,12 @@ class DQNAgent(Node):
 
         # DQN hyperparameter
         self.discount_factor = 0.99
-        self.learning_rate = 0.00025
+        self.learning_rate = 0.0007
         self.epsilon = 1.0
         self.step_counter = 0
         self.epsilon_decay = 20000 * self.stage
         self.epsilon_min = 0.05
-        self.batch_size = 64
+        self.batch_size = 128
 
         # Replay memory
         self.replay_memory = collections.deque(maxlen=500000)
@@ -229,12 +229,18 @@ class DQNAgent(Node):
             model = Sequential()
             model.add(Dense(512, input_shape=(self.state_size,), activation='relu'))
             model.add(Dense(256, activation='relu'))
-            #model.add(Dense(256, activation='relu'))
+            model.add(Dense(256, activation='relu'))
+            model.add(Dropout(0.2))
+
             model.add(Dense(128, activation='relu'))
-            #model.add(Dense(128, activation='relu'))
+            model.add(Dense(128, activation='relu'))
+            model.add(Dropout(0.2))
+
             model.add(Dense(64, activation='relu'))
-            #model.add(Dense(64, activation='relu'))
-            model.add(Dense(32, activation='relu'))
+            model.add(Dense(64, activation='relu'))
+            model.add(Dropout(0.2))
+
+            #model.add(Dense(32, activation='relu'))
             model.add(Dense(self.action_size, activation='linear'))
             model.compile(loss='mse', optimizer=Adam(lr=self.learning_rate))
             model.summary()
